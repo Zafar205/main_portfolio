f19f4c43b301bae118ae876b2e567e95c880cac1
Zafar205 <142920742+Zafar205@users.noreply.github.com>
2024-11-04 15:31:34 +0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
       className="flex flex-col items-center justify-center py-20"
       id="projects"
     >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 z-[50]">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
 
