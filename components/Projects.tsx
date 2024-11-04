84bd3d8cce697e036a6e524251fe8a2ab99acdb2
Zafar205 <142920742+Zafar205@users.noreply.github.com>
2024-11-04 15:33:02 +0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ const Projects = () => {
   const link = "https://notetaker123.vercel.app";
   return (
     <div
-      className="flex flex-col items-center justify-center py-20 z-[20]"
+      className="flex flex-col items-center justify-center py-20 z-[100]"
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
