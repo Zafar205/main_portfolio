d75a9a83bf1a634f6e12df40f171723768211800
Zafar205 <142920742+Zafar205@users.noreply.github.com>
2024-11-04 15:30:39 +0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,12 +9,12 @@ const Projects = () => {
       className="flex flex-col items-center justify-center py-20"
       id="projects"
     >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 z-[50]">
         My Projects
       </h1>
 
       <div className="h-full w-full grid gap-10 px-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        <a href={link} target="_blank" rel="noopener noreferrer" style={{ zIndex: 100 }}>
+        <a href={link} target="_blank" rel="noopener noreferrer" >
           <ProjectCard
             src="/notetaker.jpg"
             title="AI Note Taking App"
@@ -22,7 +22,7 @@ const Projects = () => {
           />
         </a>
 
-        <a href={link} target="_blank" rel="noopener noreferrer" style={{ zIndex: 100 }}>
+        <a href={link} target="_blank" rel="noopener noreferrer" >
           <ProjectCard
             src="/CardImage.png"
             title="Interactive Website Cards"
@@ -30,7 +30,7 @@ const Projects = () => {
           />
         </a>
 
-        <a href={link} target="_blank" rel="noopener noreferrer" style={{ zIndex: 100 }}>
+        <a href={link} target="_blank" rel="noopener noreferrer" >
           <ProjectCard
             src="/SpaceWebsite.png"
             title="Space Themed Website"
@@ -38,7 +38,7 @@ const Projects = () => {
           />
         </a>
 
-        <a href={link} target="_blank" rel="noopener noreferrer" style={{ zIndex: 100 }}>
+        <a href={link} target="_blank" rel="noopener noreferrer" >
           <ProjectCard
             src="/CardImage.png"
             title="Interactive Website Cards"
