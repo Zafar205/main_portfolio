ca4cdcf7eeb8e77cd76865923705d09c534b0040
Muhammad Zafar <142920742+Zafar205@users.noreply.github.com>
2025-03-12 13:37:06 -0700

    Update Encryption.tsx

--- components/Encryption.tsx
+++ components/Encryption.tsx
@@ -9,10 +9,7 @@ const Encryption = () => {
   return (
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
       <div className="absolute w-auto h-auto top-0 z-[5]">
-        <motion.div
-          variants={slideInFromTop}
-          className="text-[40px] font-medium text-center text-gray-200"
-        >
+        <motion.div variants={slideInFromTop} className="text-[40px] font-medium text-center text-gray-200">
           Performance
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             {" "}
